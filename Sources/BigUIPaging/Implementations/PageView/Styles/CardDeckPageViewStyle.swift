faa4cceb5d4cb04315ca247d711c33f10ffc5b29
Samuel McGarry <samuel.mcgarry@gmail.com>
2024-04-07 22:10:27 +0200

    Try delaying content touches

--- Sources/BigUIPaging/Implementations/PageView/Styles/CardDeckPageViewStyle.swift
+++ Sources/BigUIPaging/Implementations/PageView/Styles/CardDeckPageViewStyle.swift
@@ -57,7 +57,8 @@ struct CardDeckPageView: View {
         }
         .measure($containerSize)
         .scaleEffect(0.8)
-        .simultaneousGesture(longPressGesture.sequenced(before: dragGesture))
+        .delaysTouches(for: 0.1)
+        .gesture(dragGesture)
         .task {
             makePages(from: configuration.selection.wrappedValue)
         }
@@ -231,5 +232,58 @@ struct DeckPageViewStyle_Previews: PreviewProvider {
     
     public static var previews: some View {
         CardDeckExample()
+    }
+}
+
+extension View {
+    func delaysTouches(for duration: TimeInterval = 0.25, onTap action: @escaping () -> Void = {}) -> some View {
+        modifier(DelaysTouches(duration: duration, action: action))
+    }
+}
+
+fileprivate struct DelaysTouches: ViewModifier {
+    @State private var disabled = false
+    @State private var touchDownDate: Date? = nil
+    
+    var duration: TimeInterval
+    var action: () -> Void
+    
+    func body(content: Content) -> some View {
+        Button(action: action) {
+            content
+        }
+        .buttonStyle(DelaysTouchesButtonStyle(disabled: $disabled, duration: duration, touchDownDate: $touchDownDate))
+        .disabled(disabled)
+    }
+}
+
+fileprivate struct DelaysTouchesButtonStyle: ButtonStyle {
+    @Binding var disabled: Bool
+    var duration: TimeInterval
+    @Binding var touchDownDate: Date?
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .onChange(of: configuration.isPressed, perform: handleIsPressed)
+    }
+    
+    private func handleIsPressed(isPressed: Bool) {
+        if isPressed {
+            let date = Date()
+            touchDownDate = date
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + max(duration, 0)) {
+                if date == touchDownDate {
+                    disabled = true
+                    
+                    DispatchQueue.main.async {
+                        disabled = false
+                    }
+                }
+            }
+        } else {
+            touchDownDate = nil
+            disabled = false
+        }
     }
 }
