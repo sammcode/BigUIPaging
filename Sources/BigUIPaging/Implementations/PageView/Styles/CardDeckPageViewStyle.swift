0fd651e28298809391d6d41274b0b03c4bc70cd6
Samuel McGarry <samuel.mcgarry@gmail.com>
2024-04-07 21:43:37 +0200

    Make gesture simultaneous

--- Sources/BigUIPaging/Implementations/PageView/Styles/CardDeckPageViewStyle.swift
+++ Sources/BigUIPaging/Implementations/PageView/Styles/CardDeckPageViewStyle.swift
@@ -57,7 +57,7 @@ struct CardDeckPageView: View {
         }
         .measure($containerSize)
         .scaleEffect(0.8)
-        .highPriorityGesture(dragGesture)
+        .gesture(dragGesture)
         .task {
             makePages(from: configuration.selection.wrappedValue)
         }
